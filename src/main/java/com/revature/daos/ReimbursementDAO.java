6f9a69f52c8c33920a7467807343e31a6747ceac
github username <meliajm@gmail.com>
2020-08-21 19:30:28 -0500

    added daos, servlets next

--- src/main/java/com/revature/daos/ReimbursementDAO.java
+++ src/main/java/com/revature/daos/ReimbursementDAO.java
@@ -44,7 +44,7 @@ public class ReimbursementDAO implements IReimbursementDAO {
 	@Override
 	public Reimbursement findByID(int id) {
 		try (Connection conn = ConnectionUtility.getConnection()) {
-			String sql = "SELECT * FROM users WHERE reimb_id ="+id+";";
+			String sql = "SELECT * FROM reimbursement WHERE reimb_id ="+id+";";
 			Statement statement = conn.createStatement();
 			ResultSet result = statement.executeQuery(sql);
 			if (result.next()) {
@@ -71,7 +71,7 @@ public class ReimbursementDAO implements IReimbursementDAO {
 	@Override
 	public boolean addReimbursement(Reimbursement r) {
 		try (Connection conn = ConnectionUtility.getConnection()) {
-			String sql = "INSERT INTO users (reimb_amount, reimb_submitted, reimb_resolved, reimb_description, reimb_author, reimb_resolver, reimb_status_id, reimb_type_id)"
+			String sql = "INSERT INTO reimbursement (reimb_amount, reimb_submitted, reimb_resolved, reimb_description, reimb_author, reimb_resolver, reimb_status_id, reimb_type_id)"
 					+ "VALUES (?, ?, ?, ?, ?, ?, ? ,?);";
 			PreparedStatement statement = conn.prepareStatement(sql);
 			
@@ -96,6 +96,26 @@ public class ReimbursementDAO implements IReimbursementDAO {
 
 	@Override
 	public boolean updateReimbursement(Reimbursement r) {
+		try (Connection conn = ConnectionUtility.getConnection()) {
+			String sql = "UPDATE reimbursement SET reimb_amount = ?, reimb_submitted = ?, reimb_resolved = ?, reimb_description = ?, reimb_author = ?, reimb_resolver =?, reimb_status_id = ?, reimb_type_id =?, WHERE reimb_id = ?;";
+			
+			PreparedStatement statement = conn.prepareStatement(sql);
+			
+			int index = 0;
+			statement.setDouble(++index, r.getReimbAmount());
+			statement.setString(++index, r.getReimbSubmitted());
+			statement.setString(++index, r.getReimbResolved());
+			statement.setString(++index, r.getReimbDescription());
+			statement.setInt(++index, r.getReimbAuthor());
+			statement.setInt(++index, r.getReimbResolver());
+			statement.setInt(++index, r.getReimbStatusID());
+			statement.setInt(++index, r.getReimbTypeID());
+			
+			statement.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
